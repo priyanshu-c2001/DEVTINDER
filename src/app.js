291de25d2ab4d8d600f174da77e8b0850a7e248d
Priyanshu <chaurasiyapriyanshu66@gmail.com>
2025-06-28 23:41:03 +0530

    API's Created

--- src/app.js
+++ src/app.js
@@ -3,21 +3,80 @@ const {connectDB} = require("./config/database");
 const app = express();
 const {User}=require("./models/user");
 
-app.post("/signup", async(req, res)=>{
-  const user=new User({
-    firstName: "Priyanshu",
-    lastName: "Chaurasia",
-    emailId: "pc.0o05x@gmail.com",
-    password: "priyanshu@123"
-  });
+app.use(express.json());
 
-  try{
-    await user.save();
-    res.send("User Added successfully!");
+app.post("/signup", async(req, res)=>{
+   const body=req.body;
+   const user= new User(body);
+
+   try{
+     await user.save();
+     res.send("User Added successfully!");
   } catch(err){
     res.status(400).send("Error saving the user:" + err.message);
   }
 });
+
+// Get user by email
+app.get('/user', async(req, res)=>{
+  const userEmail=req.body.emailId;
+  try{
+    const user=await User.findOne({emailId: userEmail});
+    if(!user.length) res.send("User not Found!!");
+    else res.send(user);
+  }
+  // try{
+  //   const user=await User.find({emailId: userEmail});
+  //   if(!user.length) res.send("User not Found!!");
+  //   else res.send(user);
+  // }
+  catch(err){
+    res.status(400).send("Something Went Wrong!!");
+    console.log(err);
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get('/feed', async (req, res)=>{
+  try{
+    const users=await User.find({});
+    if(!users.length) res.send("No User Exist!!");
+    else res.send(users);
+  }
+  catch(err){
+    res.status(400).send("Something Went Wrong!!");
+  }
+});
+
+// Delete a user from the database
+app.delete('/user', async(req, res)=>{
+  const userId=req.body.userId;
+
+  try{
+    const user=await User.findByIdAndDelete(userId);
+    if(user==null) res.send("User Not Found!!");
+    else res.send("User Deleted Successfully!!");
+  }
+   catch(err){
+    res.status(400).send("Something Went Wrong!!");
+  }
+});
+
+// Update data of the user
+app.patch('/user', async(req, res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+
+  try{
+    const user= await User.findByIdAndUpdate(userId, data, {returnDocument: "after"});
+    console.log(user);
+    res.send("User updated Successfully!!");
+  }
+  catch(err){
+    res.status(400).send("Something Went Wrong!!");
+  }
+});
+
 
 connectDB().then(() => {
   console.log("Database connection established...");
